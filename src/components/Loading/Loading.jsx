5d84bcc825cf637385133d7b8bb32a559e0260b5
Jackeline Castro <jackelinecastrocardoso@hotmail.com>
2023-07-20 20:32:37 -0300

    estilizacao hero

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,23 +1,15 @@
 import BeatLoader from "react-spinners/BeatLoader";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./Loading.css"
 
 
 const LoadingPage = () => {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 35000);
-  }, [])
-
+  
   return (
     <div className="loading-container">
-       {loading && (
-         <BeatLoader size={15} color={"#ac36d6"} loading={loading} /> 
-        )}
+       
+         <BeatLoader size={15} color={"#ac36d6"} loading={true} /> 
+        
     </div>
   )
 }
